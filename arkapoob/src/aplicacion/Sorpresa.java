a553908e6af9006f0ff64e717527af7e9d3d6102
DamGaFlo <jd98091819@gmail.com>
2019-05-07 08:15:07 -0500

    prueba

--- arkapoob/src/aplicacion/Sorpresa.java
+++ arkapoob/src/aplicacion/Sorpresa.java
@@ -18,76 +18,36 @@ public abstract class Sorpresa extends GameObject{
 	private Bloque bloque;
 	private boolean destruida = false;
 	
-        /**
-         * 
-         * @param posicion - posicion de la sorpresa
-         * @param textura - imagen que define el tipo de sorpresa
-         * @param bloque - Bloque que contiene la sorpresa
-         */
 	public Sorpresa(Vector2D posicion,BufferedImage textura,Bloque bloque){
 		super(posicion,textura);
 		this.bloque = bloque;
 	}
 	
-        
 	public abstract void efecto(Base base);
 	
-        /**
-         * Genera el movimiento de la sorpresa al caer
-         */
 	public void caer(){
 		setPosicion(getPosicion().add(velocidad));
 	}
-        
-        /**
-         * Modifica la visibilidad de la sorpresa
-         * @param b - define si es visible o no
-         */
 	public void setVisible(boolean b){
 		visible = b;
 	}
-        /**
-         * Dibuja la sorpresa en una posicion respectiva
-         * @param g 
-         */
-        @Override
 	public void draw(Graphics g){
 		if(visible) g.drawImage(getTextura(),(int)getPosicion().getX(),(int) getPosicion().getY(),null);
 	}
-        /**
-         * Actualiza el comportamiento de la sorpresa
-         */
-        @Override
 	public void update(){
 		if(visible){
 			caer();
 			colicion();
 		}
 	}
-        /**
-         * 
-         * @return Juego sobre el cual actua
-         */
 	public ArkaPOOB getArkaPoob(){
 		return bloque.getArkaPOOB();
 	}
-	/**
-         * 
-         * @param posicion - Posicion de la sospresa
-         * @param bloque - bloque que la contiene
-         * @return tipo de sorpresa aleatoria
-         */
+	
 	public static Sorpresa randomSorpresa(Vector2D posicion,Bloque bloque){
 		Random random = new Random();
 		return getSorpresa(random.nextInt(NUM_SORPRESAS)+1,posicion,bloque);
 	} 
-        /**
-         * 
-         * @param numPower - poder que posee la sorpresa
-         * @param posicion - posicion en la cual estara ubicada
-         * @param bloque - bloque que la contiene
-         * @return Sorpresa con el tipo de poder
-         */
 	public static Sorpresa getSorpresa(int numPower,Vector2D posicion,Bloque bloque){
 		Sorpresa envio;
 		switch(numPower){
@@ -102,19 +62,11 @@ public abstract class Sorpresa extends GameObject{
 		}
 		return envio;
 	}
-        
-        /**
-         * Define el comportamiento de la sospresa al salir del borde de juego
-         */
 	public void bordes(){
 		if(getCentro().getY()+getHeight()/2 >= getMaxHight()){
 			destroy();
 		}
 	}
-        
-        /**
-         * Define el comportamiento al empezar a caer
-         */
 	public void colicion(){
 		bordes();
 		if(!destruida){
@@ -122,9 +74,6 @@ public abstract class Sorpresa extends GameObject{
 		}
 		
 	}
-        /**
-         * define el comportamiento al ser atrapada por un jugador
-         */
 	public void colicionPlayers(){
 		ArrayList<Player> players = getArkaPoob().getPlayers();
 		Vector2D miCentro = getCentro();
@@ -139,25 +88,13 @@ public abstract class Sorpresa extends GameObject{
 			}
 		}
 	}
-        
-        /**
-         * Destruye la sorpresa
-         */
 	public void destroy(){
 		getArkaPoob().getSorpresas().remove(this);
 		destruida = true;
 	}
-        /**
-         * 
-         * @return Ancho maximo del juego
-         */
 	public int getMaxWidth(){
 		return getArkaPoob().getWidth();
 	}
-        /**
-         * Alto maximo de juego
-         * @return 
-         */
 	public int getMaxHight(){
 		return getArkaPoob().getHight();
 	}
--- arkapoob/src/graficos/Recursos.java
+++ arkapoob/src/graficos/Recursos.java
@@ -6,10 +6,6 @@ import java.awt.image.BufferedImage;
 public class Recursos{
 	public static BufferedImage bola,plataforma,bloqueRojo,marco,fastBall,slowBall,bloqueSorpresa,corazon,arka,bloqueDuro,bloqueIndestructible;
 	public static BufferedImage[] numeros = new BufferedImage[11];
-        
-        /**
-         * Inicia la carga de imagenes
-         */
 	public static void init(){
 		bola = Loader.ImageLoader("recursos/balls/ball.png");
 		plataforma = Loader.ImageLoader("recursos/plataformas/plataforma.png");
